7bd96873ea7d891088940825277f7048c20d1ce2
Dan Naiger <dannitzhak@gmail.com>
2026-03-02 11:14:02 +0200

    refactor: extract parse_a_instruction and parse_c_instruction

--- project6/src/project6/__init__.py
+++ project6/src/project6/__init__.py
@@ -13,9 +13,11 @@ class CInstruction:
     jump: str | None
 
 
-def parse(line: str) -> AInstruction | CInstruction:
-    if line.startswith("@"):
-        return AInstruction(number=int(line[1:]))
+def parse_a_instruction(line: str) -> AInstruction:
+    return AInstruction(number=int(line[1:]))
+
+
+def parse_c_instruction(line: str) -> CInstruction:
     if "=" in line:
         dest, rest = line.split("=")
     else:
@@ -27,6 +29,12 @@ def parse(line: str) -> AInstruction | CInstruction:
         comp = rest
         jump = None
     return CInstruction(dest=dest, comp=comp, jump=jump)
+
+
+def parse(line: str) -> AInstruction | CInstruction:
+    if line.startswith("@"):
+        return parse_a_instruction(line)
+    return parse_c_instruction(line)
 
 
 def main() -> None:
