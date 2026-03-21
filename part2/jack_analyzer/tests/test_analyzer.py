434dc28b88d02a388af6854511754c21519e5d50
Dan Naiger <dannitzhak@gmail.com>
2026-03-21 15:29:40 +0200

    change names of tmpfiles

--- part2/jack_analyzer/tests/test_analyzer.py
+++ part2/jack_analyzer/tests/test_analyzer.py
@@ -14,28 +14,28 @@ def test_analyzer_writes_only_token_for_empty_file(tmp_path):
 </tokens>"""
 
 def test_has_more_tokens_is_false_on_spaces_and_newlines(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text("        \n  \n ")
     with open(file) as f:
         tokenizer=JackTokenizer(f)
         assert tokenizer.has_more_tokens()==False
 
 def test_has_more_tokens_is_true_for_class(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text("class")
     with open(file) as f:
         tokenizer=JackTokenizer(f)
         assert tokenizer.has_more_tokens()==True
 
 def test_has_more_tokens_is_false_for_inline_comment(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text(" \n   // an inline comment ")
     with open(file) as f:
         tokenizer=JackTokenizer(f)
         assert tokenizer.has_more_tokens()==False
 
 def test_current_token_updated_to_class_after_advancing(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text("class")
     with open(file) as f:
         tokenizer=JackTokenizer(f)
@@ -43,7 +43,7 @@ def test_current_token_updated_to_class_after_advancing(tmp_path):
         assert tokenizer.get_current_token()=="class"
 
 def test_has_more_tokens_before_adavancing_and_has_not_after(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text("class")
     with open(file) as f:
         tokenizer=JackTokenizer(f)
@@ -53,7 +53,7 @@ def test_has_more_tokens_before_adavancing_and_has_not_after(tmp_path):
         assert tokenizer.has_more_tokens()==False
 
 def test_can_infer_class_token_while_ignoring_preceding_newlines_spaces_and_inline_comments(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text("""    //some_comment
     //more comments
                     class""")
@@ -65,7 +65,7 @@ def test_can_infer_class_token_while_ignoring_preceding_newlines_spaces_and_inli
         assert tokenizer.has_more_tokens()==False
 
 def test_can_infer_class_token_while_ignoring_trailing_newlines_spaces_and_inline_comments(tmp_path):
-    file = tmp_path /"EmptyFile.jack"
+    file = tmp_path /"File.jack"
     file.write_text("""class //a comment
            //another comment
                              """)
@@ -75,3 +75,5 @@ def test_can_infer_class_token_while_ignoring_trailing_newlines_spaces_and_inlin
         tokenizer.advance()
         assert tokenizer.get_current_token()=="class"
         assert tokenizer.has_more_tokens()==False
+
+
