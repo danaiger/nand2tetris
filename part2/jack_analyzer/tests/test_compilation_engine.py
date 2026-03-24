dc19c89b8acca5ce9b3fa345faefcc9208e2a1aa
Dan Naiger <dannitzhak@gmail.com>
2026-03-24 02:34:16 +0200

    add test for multiple class var decleration in a row

--- part2/jack_analyzer/tests/test_compilation_engine.py
+++ part2/jack_analyzer/tests/test_compilation_engine.py
@@ -35,3 +35,32 @@ def test_basic_class_var_dec(tmp_path):
   </classVarDec>
   <symbol> } </symbol>
 </class>"""
+
+def test_multiple_basic_class_var_dec(tmp_path):
+    input_path = tmp_path /"input_file"
+    output_path = tmp_path /"output_file"
+    input_path.write_text("""class SomeClass {
+   field int size; 
+   field int x; 
+                          }""")
+    with open(input_path) as input_file:
+        with open(output_path,"w") as output_file:
+            CompilationEngine(input_file,output_file)
+    assert output_path.read_text()== """<class>
+  <keyword> class </keyword>
+  <identifier> SomeClass </identifier>
+  <symbol> { </symbol>
+  <classVarDec>
+    <keyword> field </keyword>
+    <keyword> int </keyword>
+    <identifier> size </identifier>
+    <symbol> ; </symbol>
+  </classVarDec>
+  <classVarDec>
+    <keyword> field </keyword>
+    <keyword> int </keyword>
+    <identifier> x </identifier>
+    <symbol> ; </symbol>
+  </classVarDec>
+  <symbol> } </symbol>
+</class>"""
