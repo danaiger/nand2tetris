090bc66097c85c0f34340fa2fbff3e75e2680b4a
Dan Naiger <dannitzhak@gmail.com>
2026-03-24 02:18:08 +0200

    add single and basic var dec

--- part2/jack_analyzer/src/jack_analyzer/compilation_engine.py
+++ part2/jack_analyzer/src/jack_analyzer/compilation_engine.py
@@ -6,18 +6,31 @@ class CompilationEngine:
         self.input_file=input_file
         self.output_file=output_file
         self.tokenizer=JackTokenizer(input_file)
+        self.indentation_spaces=0
         self.compile_class()
 
     def compile_atom(self):
-        self.output_file.write(f"  <{self.tokenizer.token_type().value}> {self.tokenizer.get_current_token()} </{self.tokenizer.token_type().value}>\n")
+        self.output_file.write(f"{' '*self.indentation_spaces}<{self.tokenizer.token_type().value}> {self.tokenizer.get_current_token()} </{self.tokenizer.token_type().value}>\n")
     
     def compile_class_var_dec(self):
-        pass
+        self.output_file.write("  <classVarDec>\n")
+        self.indentation_spaces+=2
+        self.compile_atom()
+        self.tokenizer.advance()
+        self.compile_atom()
+        self.tokenizer.advance()
+        self.compile_atom()
+        self.tokenizer.advance()
+        self.compile_atom()
+        self.indentation_spaces-=2
+        self.output_file.write("  </classVarDec>\n")
+
     def compile_subroutine_dec(self):
         pass
 
     def compile_class(self):
         self.output_file.write("<class>\n")
+        self.indentation_spaces+=2
         self.tokenizer.advance()
         self.compile_atom() #class keyword
         self.tokenizer.advance()
@@ -32,4 +45,5 @@ class CompilationEngine:
             self.compile_subroutine_dec()
             self.tokenizer.advance()
         self.compile_atom()
+        self.indentation_spaces-=2
         self.output_file.write("</class>")
--- part2/jack_analyzer/tests/test_compilation_engine.py
+++ part2/jack_analyzer/tests/test_compilation_engine.py
@@ -13,3 +13,25 @@ def test_class_without_class_var_dec_and_without_subroutine(tmp_path):
   <symbol> { </symbol>
   <symbol> } </symbol>
 </class>"""
+
+def test_basic_class_var_dec(tmp_path):
+    input_path = tmp_path /"input_file"
+    output_path = tmp_path /"output_file"
+    input_path.write_text("""class SomeClass {
+   field int size; 
+                          }""")
+    with open(input_path) as input_file:
+        with open(output_path,"w") as output_file:
+            CompilationEngine(input_file,output_file)
+    assert output_path.read_text()== """<class>
+  <keyword> class </keyword>
+  <identifier> SomeClass </identifier>
+  <symbol> { </symbol>
+  <classVarDec>
+    <keyword> field </keyword>
+    <keyword> int </keyword>
+    <identifier> size </identifier>
+    <symbol> ; </symbol>
+  </classVarDec>
+  <symbol> } </symbol>
+</class>"""
