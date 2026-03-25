a5560f98f9e2e19e8bcb06b6b31f2cb36b1482a8
Dan Naiger <dannitzhak@gmail.com>
2026-03-25 20:05:45 +0200

    remove unnecessary fixture

--- part2/jack_analyzer/tests/test_analyzer.py
+++ part2/jack_analyzer/tests/test_analyzer.py
@@ -23,14 +23,14 @@ def test_dirs_tokenize_as_expected(dir_name):
         actual_file = expected_file.with_name(expected_file.name.removeprefix("expected_"))
         assert actual_file.read_text() == expected_file.read_text()
     
-def test_analyzing_correctly_an_expressionless_file(tmp_path):
+def test_analyzing_correctly_an_expressionless_file():
     analyzer=JackAnalyzer(TEST_DIR/"ExpressionLessSquare/Main.jack")
     analyzer.analyze()
     analyzed_file_path=(TEST_DIR/"ExpressionLessSquare/Main.xml")
     expected_file_path=(TEST_DIR/"ExpressionLessSquare/expected_Main.xml")
     assert expected_file_path.read_text()==analyzed_file_path.read_text()
     
-def test_analyzing_correctly_a_dir_with_only_expressionless_files(tmp_path):
+def test_analyzing_correctly_a_dir_with_only_expressionless_files():
     current_dir=TEST_DIR/"ExpressionLessSquare"
     analyzer=JackAnalyzer(current_dir)
     analyzer.analyze()
