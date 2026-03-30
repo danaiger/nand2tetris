5cab59ec5f9cfd429c179e2e22605ebc474f1a93
Dan Naiger <dannitzhak@gmail.com>
2026-03-30 17:04:00 +0300

    test var dec extended xml (already working)

--- part2/jack_analyzer/tests/test_compilation_engine_extended_xml.py
+++ part2/jack_analyzer/tests/test_compilation_engine_extended_xml.py
@@ -226,102 +226,102 @@ def test_subroutine_with_parameters_list(tmp_path):
 </class>
 """
 
-# def test_subroutine_with_var_declaration(tmp_path):
-#     input_path = tmp_path /"input_file"
-#     output_path = tmp_path /"output_file"
-#     input_path.write_text("""class SomeClass {
-#     method void draw() {
-#         var char key;
-#         return;
-#                     }
-#     }""")
-#     with open(input_path) as input_file:
-#         with open(output_path,"w") as output_file:
-#             CompilationEngine(input_file,XMLWriter(output_file))
-#     assert output_path.read_text()== """<class>
-#   <keyword> class </keyword>
-#   <identifier> SomeClass </identifier>
-#   <symbol> { </symbol>
-#   <subroutineDec>
-#     <keyword> method </keyword>
-#     <keyword> void </keyword>
-#     <identifier> draw </identifier>
-#     <symbol> ( </symbol>
-#     <parameterList>
-#     </parameterList>
-#     <symbol> ) </symbol>
-#     <subroutineBody>
-#       <symbol> { </symbol>
-#       <varDec>
-#         <keyword> var </keyword>
-#         <keyword> char </keyword>
-#         <identifier> key </identifier>
-#         <symbol> ; </symbol>
-#       </varDec>
-#       <statements>
-#         <returnStatement>
-#           <keyword> return </keyword>
-#           <symbol> ; </symbol>
-#         </returnStatement>
-#       </statements>
-#       <symbol> } </symbol>
-#     </subroutineBody>
-#   </subroutineDec>
-#   <symbol> } </symbol>
-# </class>
-# """
+def test_subroutine_with_var_declaration(tmp_path):
+    input_path = tmp_path /"input_file"
+    output_path = tmp_path /"output_file"
+    input_path.write_text("""class SomeClass {
+    method void draw() {
+        var char key;
+        return;
+                    }
+    }""")
+    with open(input_path) as input_file:
+        with open(output_path,"w") as output_file:
+            CompilationEngine(input_file,ExtendedXMLWriter(output_file))
+    assert output_path.read_text()== """<class>
+  <keyword> class </keyword>
+  <identifier class-definition> SomeClass </identifier class-definition>
+  <symbol> { </symbol>
+  <subroutineDec>
+    <keyword> method </keyword>
+    <keyword> void </keyword>
+    <identifier subroutine-definition> draw </identifier subroutine-definition>
+    <symbol> ( </symbol>
+    <parameterList>
+    </parameterList>
+    <symbol> ) </symbol>
+    <subroutineBody>
+      <symbol> { </symbol>
+      <varDec>
+        <keyword> var </keyword>
+        <keyword> char </keyword>
+        <identifier var-definition-0> key </identifier var-definition-0>
+        <symbol> ; </symbol>
+      </varDec>
+      <statements>
+        <returnStatement>
+          <keyword> return </keyword>
+          <symbol> ; </symbol>
+        </returnStatement>
+      </statements>
+      <symbol> } </symbol>
+    </subroutineBody>
+  </subroutineDec>
+  <symbol> } </symbol>
+</class>
+"""
 
-# def test_subroutine_with_multiple_var_declarations(tmp_path):
-#     input_path = tmp_path /"input_file"
-#     output_path = tmp_path /"output_file"
-#     input_path.write_text("""class SomeClass {
-#     method void draw() {
-#         var char key;
-#         var boolean exit;
-#         return;
-#                     }
-#     }""")
-#     with open(input_path) as input_file:
-#         with open(output_path,"w") as output_file:
-#             CompilationEngine(input_file,XMLWriter(output_file))
-#     assert output_path.read_text()== """<class>
-#   <keyword> class </keyword>
-#   <identifier> SomeClass </identifier>
-#   <symbol> { </symbol>
-#   <subroutineDec>
-#     <keyword> method </keyword>
-#     <keyword> void </keyword>
-#     <identifier> draw </identifier>
-#     <symbol> ( </symbol>
-#     <parameterList>
-#     </parameterList>
-#     <symbol> ) </symbol>
-#     <subroutineBody>
-#       <symbol> { </symbol>
-#       <varDec>
-#         <keyword> var </keyword>
-#         <keyword> char </keyword>
-#         <identifier> key </identifier>
-#         <symbol> ; </symbol>
-#       </varDec>
-#       <varDec>
-#         <keyword> var </keyword>
-#         <keyword> boolean </keyword>
-#         <identifier> exit </identifier>
-#         <symbol> ; </symbol>
-#       </varDec>
-#       <statements>
-#         <returnStatement>
-#           <keyword> return </keyword>
-#           <symbol> ; </symbol>
-#         </returnStatement>
-#       </statements>
-#       <symbol> } </symbol>
-#     </subroutineBody>
-#   </subroutineDec>
-#   <symbol> } </symbol>
-# </class>
-# """
+def test_subroutine_with_multiple_var_declarations(tmp_path):
+    input_path = tmp_path /"input_file"
+    output_path = tmp_path /"output_file"
+    input_path.write_text("""class SomeClass {
+    method void draw() {
+        var char key;
+        var boolean exit;
+        return;
+                    }
+    }""")
+    with open(input_path) as input_file:
+        with open(output_path,"w") as output_file:
+            CompilationEngine(input_file,ExtendedXMLWriter(output_file))
+    assert output_path.read_text()== """<class>
+  <keyword> class </keyword>
+  <identifier class-definition> SomeClass </identifier class-definition>
+  <symbol> { </symbol>
+  <subroutineDec>
+    <keyword> method </keyword>
+    <keyword> void </keyword>
+    <identifier subroutine-definition> draw </identifier subroutine-definition>
+    <symbol> ( </symbol>
+    <parameterList>
+    </parameterList>
+    <symbol> ) </symbol>
+    <subroutineBody>
+      <symbol> { </symbol>
+      <varDec>
+        <keyword> var </keyword>
+        <keyword> char </keyword>
+        <identifier var-definition-0> key </identifier var-definition-0>
+        <symbol> ; </symbol>
+      </varDec>
+      <varDec>
+        <keyword> var </keyword>
+        <keyword> boolean </keyword>
+        <identifier var-definition-1> exit </identifier var-definition-1>
+        <symbol> ; </symbol>
+      </varDec>
+      <statements>
+        <returnStatement>
+          <keyword> return </keyword>
+          <symbol> ; </symbol>
+        </returnStatement>
+      </statements>
+      <symbol> } </symbol>
+    </subroutineBody>
+  </subroutineDec>
+  <symbol> } </symbol>
+</class>
+"""
 
 # def test_return_with_degenerate_expression(tmp_path):
 #     input_path = tmp_path /"input_file"
