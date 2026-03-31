9fc74996ec1f5abcd23197f255970d7267a35af3
Dan Naiger <dannitzhak@gmail.com>
2026-04-01 01:23:39 +0300

    parenthesis cause compilatiion in right order, multiplication operation are compiled as call to os

--- part2/jack_analyzer/tests/test_compilation_engine_vm.py
+++ part2/jack_analyzer/tests/test_compilation_engine_vm.py
@@ -79,3 +79,28 @@ push constant 0
 return
 """
 
+
+def test_expressions_in_paranthesis_are_compiled_in_right_order(tmp_path):
+    input_path = tmp_path /"input_file"
+    output_path = tmp_path /"output_file"
+    input_path.write_text("""class Main {
+        function void main(){
+          do Output.printInt(1+(2*3));
+                return;
+                      }
+                          }""")
+    with open(input_path) as input_file:
+        with open(output_path,"w") as output_file:
+            CompilationEngine(input_file,VMWriter(output_file))
+    assert output_path.read_text()=="""function Main.main 0
+push constant 1
+push constant 2
+push constant 3
+call Math.multiply 2
+add
+call Output.printInt 1
+pop temp 0
+push constant 0
+return
+"""
+
