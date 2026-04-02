675bf7a46317c0b0ceddab8807755ab99934aa90
Dan Naiger <dannitzhak@gmail.com>
2026-04-02 05:32:21 +0300

    test ConvertToBin passes

--- part2/jack_analyzer/tests/test_compiler.py
+++ part2/jack_analyzer/tests/test_compiler.py
@@ -6,7 +6,7 @@ from jack_analyzer.compiler import JackCompiler
 TEST_DIR=Path("tests/test_data/Compiler")
 
 
-@pytest.mark.parametrize("dir_name", ["Seven"])
+@pytest.mark.parametrize("dir_name", ["Seven","ConvertToBin"])
 def test_dirs_analyzed_as_expected(dir_name, tmp_path):
     current_dir = TEST_DIR / dir_name
     compiler = JackCompiler(current_dir)
